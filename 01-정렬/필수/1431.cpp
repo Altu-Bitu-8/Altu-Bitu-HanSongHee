3e6da5e5ea4d44fb37181b5155c294d7d91aad0c
song122333 <songewha23@ewha.ac.kr>
2025-02-20 23:39:10 +0900

    [정렬] 2월 20일

--- 01-정렬/필수/1431.cpp
+++ 01-정렬/필수/1431.cpp
@@ -8,23 +8,7 @@ int compare(string a, string b) {
     int b_size = b.size();
     int a_sum = 0;
     int b_sum = 0;
-    if (a_size != b_size) {
-        return a_size < b_size;
-    }
-    else {
-        for (int i = 0; i < a_size; i++) {
-            if (a[i] >= '0' && a[i] <= '9') {
-                a_sum += int(a[i]) - 48;
-            }
-            if (b[i] >= '0' && b[i] <= '9') {
-                b_sum += int(b[i]) - 48;
-            }
-        }
-        if (a_sum != b_sum) {
-            return a_sum < b_sum;
-        }
-    }
-    return a < b;
+    
 }
 int main() {
     int N;
